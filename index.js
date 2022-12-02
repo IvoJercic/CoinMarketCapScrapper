6ce6e1ddccaafd0c24f8c1d5025492695bb00908
IvoJercic <ivo.jercic5@gmail.com>
2022-12-02 16:52:56 +0100

    added scrapper function

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 import express from "express";
+import autoScroll from "./autoScroll.js";
+import scrapeData from "./scrapeData.js";
+import puppeteer from "puppeteer";
 
 const app = express();
+let globals = null;
 
-app.get("/", (req, res) => {
-  res.json({
-    hello: "hi",
-  });
+app.get("/", async (req, res) => {
+  try {
+    if (!globals) {
+      let jsonData = undefined;
+      const siteUrl = "https://coinmarketcap.com/all/views/all/";
+      const browser = await puppeteer.launch({
+        headless: false,
+      });
+      const page = await browser.newPage();
+      await page.goto(siteUrl);
+
+      await autoScroll(page);
+
+      const pageData = await page.evaluate(() => {
+        return {
+          html: document.documentElement.innerHTML,
+        };
+      });
+
+      jsonData = scrapeData(pageData.html);
+      globals = jsonData;
+      await browser.close();
+      res.json(jsonData);
+    } else {
+      res.json(globals);
+    }
+  } catch (e) {
+    console.log(e);
+    res.json(null);
+  }
 });
 
 app.listen("3000", () => {
